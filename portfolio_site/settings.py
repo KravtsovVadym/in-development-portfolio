2392a14739f16e642b100bab4443f653d873c477
Vadym <vadymkravtcsov@gmail.com>
2026-01-15 22:41:11 +0200

    chore: debug

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='')
 # Add development hosts only if DEBUG is True
 if config('DEBUG', default=False, cast=bool):
